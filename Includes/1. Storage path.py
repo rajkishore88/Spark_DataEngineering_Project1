1a3578cb2c93eebff99be00927040c4f0b9788bc
rajkishore.sethi88@gmail.com <rajkishore.sethi88@gmail.com>
2023-07-24 06:25:06 +0000

    Initial version

---
+++ Includes/1. Storage path.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path="abfss://raw@adlsstreamingpocgn2.dfs.core.windows.net"
+processed_folder_path="abfss://processed@adlsstreamingpocgn2.dfs.core.windows.net"
+presentation_folder_path="abfss://presentation@adlsstreamingpocgn2.dfs.core.windows.net"
+
+# COMMAND ----------
+
+
